bef528d976bfcb6d06d0ec3a01efb2ba6b58c058
vividege <xiaohailin136@gmail.com>
2024-08-15 14:20:08 +0800

    plus one: add two solutions for reference

--- ContestChampion/contest/plus_one.py
+++ ContestChampion/contest/plus_one.py
@@ -1,5 +1,5 @@
 class BigInteger:
-    def plus_one_48ms(self, digits: list[int]) -> list[int]:
+    def plus_one(self, digits: list[int]) -> list[int]:
         add_flag = 1
         for i in range(len(digits) - 1, -1, -1):
             if add_flag == 0:
@@ -18,7 +18,19 @@ class BigInteger:
             ret = digits
         return ret
 
+    def plus_one_48ms(self, digits: list[int]) -> list[int]:
+        length = len(digits)
+        sum = 0
+        for i in range(length):
+            sum += digits[i] * pow(10,length-i-1)
+        sum += 1
+        return [int(x) for x in str(sum)]
 
-if __name__ == '__main__':
-    b = BigInteger()
-    b.plus_one_48ms([9])
+    def plus_one_fastest(self, digits: list[int]) -> list[int]:
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        return [1]+digits
--- ContestChampion/test/test_plus_one.py
+++ ContestChampion/test/test_plus_one.py
@@ -9,19 +9,59 @@ class TestBigInteger(unittest.TestCase):
 
     def test_plus_one_123(self):
         n1 = [1, 2, 3]
-        result = self.big_int.plus_one_48ms(n1)
+        result = self.big_int.plus_one(n1)
         self.assertEqual([1, 2, 4], result)
 
     def test_plus_one_9(self):
         n1 = [9]
-        result = self.big_int.plus_one_48ms(n1)
+        result = self.big_int.plus_one(n1)
         self.assertEqual([1, 0], result)
 
     def test_plus_one_9999(self):
         n1 = [9, 9, 9, 9]
-        result = self.big_int.plus_one_48ms(n1)
+        result = self.big_int.plus_one(n1)
         self.assertEqual([1, 0, 0, 0, 0], result)
     def test_plus_one_1089(self):
         n1 = [1,0,8,9]
-        result = self.big_int.plus_one_48ms(n1)
+        result = self.big_int.plus_one(n1)
         self.assertEqual([1,0,9,0], result)
+
+    def test_plus_one48ms_123(self):
+        n1 = [1, 2, 3]
+        result = self.big_int.plus_one_48ms(n1)
+        self.assertEqual([1, 2, 4], result)
+
+    def test_plus_one48ms_9(self):
+        n1 = [9]
+        result = self.big_int.plus_one_48ms(n1)
+        self.assertEqual([1, 0], result)
+
+    def test_plus_one48ms_9999(self):
+        n1 = [9, 9, 9, 9]
+        result = self.big_int.plus_one_48ms(n1)
+        self.assertEqual([1, 0, 0, 0, 0], result)
+
+    def test_plus_one48ms_1089(self):
+        n1 = [1, 0, 8, 9]
+        result = self.big_int.plus_one_48ms(n1)
+        self.assertEqual([1, 0, 9, 0], result)
+
+    def test_plus_onefast_123(self):
+        n1 = [1, 2, 3]
+        result = self.big_int.plus_one_fastest(n1)
+        self.assertEqual([1, 2, 4], result)
+
+    def test_plus_onefast_9(self):
+        n1 = [9]
+        result = self.big_int.plus_one_fastest(n1)
+        self.assertEqual([1, 0], result)
+
+    def test_plus_onefast_9999(self):
+        n1 = [9, 9, 9, 9]
+        result = self.big_int.plus_one_fastest(n1)
+        self.assertEqual([1, 0, 0, 0, 0], result)
+
+    def test_plus_onefast_1089(self):
+        n1 = [1, 0, 8, 9]
+        result = self.big_int.plus_one_fastest(n1)
+        self.assertEqual([1, 0, 9, 0], result)
